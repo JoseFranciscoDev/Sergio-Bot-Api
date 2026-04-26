71df6710f0a2b736cb5c43e48b7b520ebf4e9076
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-04-26 20:32:34 -0300

    cleanup: remove import unused in schemas.py

--- api/v1/Users/schemas.py
+++ api/v1/Users/schemas.py
@@ -1,8 +1,8 @@
-from sqlalchemy.sql.operators import ge
 from typing import Literal
 from pydantic import BaseModel, Field
 
+
 class FiltersSchema(BaseModel):
-    limit: int = Field(10,ge= 10, le= 100 )
+    limit: int = Field(10, ge=10, le=100)
     page: int = Field(default=1, ge=1)
     order_by: Literal["name", "id"] = "id"
