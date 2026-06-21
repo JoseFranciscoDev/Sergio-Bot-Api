d2ff16303a2ee47718eda6a68fcea39bf3da33de
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-06-21 15:56:25 -0300

    feat: add alembic to migrations managing

--- api/v1/shared/database.py
+++ api/v1/shared/database.py
@@ -3,14 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from api.v1.shared.settings import settings
 from api.v1.shared.base import Base  # noqa: F401
 from api.v1.Users.models import *  # noqa: F401, F403
+from api.v1.Conversation.models import *  # noqa: F401, F403
 from api.v1.Message.models import *  # noqa: F401, F403
 
 engine = create_engine(settings.DB_URL)
 Session = sessionmaker(engine)
-
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
 
 def get_db():
